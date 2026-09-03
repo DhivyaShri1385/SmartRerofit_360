4fe29b12b5d61aabc80e6bc8dd9289a737eb25fa
Dhivya Shri <dhivyashri1385@gmail.com>
2026-09-03 11:14:09 +0530

    Add Alerts.jsx

---
+++ frontend/src/pages/Alerts.jsx
@@ -0,0 +1,12 @@
+// frontend/src/pages/Alerts.jsx
+import PageHeader from "../components/PageHeader";
+import PlaceholderPanel from "../components/PlaceholderPanel";
+
+export default function Alerts() {
+  return (
+    <div>
+      <PageHeader title="Alerts & Notifications" />
+      <PlaceholderPanel label="Alerts" />
+    </div>
+  );
+}
