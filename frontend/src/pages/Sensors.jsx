add5df4e0ef043260daf990218e531fc3f099216
Dhivya Shri <dhivyashri1385@gmail.com>
2026-09-03 11:14:10 +0530

    Add Sensors.jsx

---
+++ frontend/src/pages/Sensors.jsx
@@ -0,0 +1,12 @@
+// frontend/src/pages/Sensors.jsx
+import PageHeader from "../components/PageHeader";
+import PlaceholderPanel from "../components/PlaceholderPanel";
+
+export default function Sensors() {
+  return (
+    <div>
+      <PageHeader title="Sensor Monitoring" />
+      <PlaceholderPanel label="Sensors" />
+    </div>
+  );
+}
