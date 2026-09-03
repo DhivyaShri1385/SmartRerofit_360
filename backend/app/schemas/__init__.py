1f8b97409027ae25d679a7db00b3536e88514546
Dhivya Shri <dhivyashri1385@gmail.com>
2026-09-03 11:14:05 +0530

    Add __init__.py

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from app.schemas.ml_training import DatasetSummary, TrainRequest, ModelRunOut
+from app.schemas.machine import MachineBase, MachineCreate, MachineUpdate, MachineOut
+
+__all__ = [
+    "DatasetSummary", "TrainRequest", "ModelRunOut",
+    "MachineBase", "MachineCreate", "MachineUpdate", "MachineOut",
+]
