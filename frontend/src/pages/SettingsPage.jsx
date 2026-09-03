f016115568237ccd823f7bce47b034e5410fd4cd
Dhivya Shri <dhivyashri1385@gmail.com>
2026-09-03 11:14:10 +0530

    Add SettingsPage.jsx

---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,12 @@
+// frontend/src/pages/SettingsPage.jsx
+import PageHeader from "../components/PageHeader";
+import PlaceholderPanel from "../components/PlaceholderPanel";
+
+export default function SettingsPage() {
+  return (
+    <div>
+      <PageHeader title="System Settings" />
+      <PlaceholderPanel label="Settings" />
+    </div>
+  );
+}
